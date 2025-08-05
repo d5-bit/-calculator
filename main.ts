a04544a65c8331cafc054908edbdd78513939184
d5-bit <d5@choup.es.kr>
2025-08-05 10:43:18 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+input.onGesture(Gesture.TiltRight, function () {
+    value2 += -1
+    DISPLAY_SUM_VALUE()
+})
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    value1 += -1
+    DISPLAY_SUM_VALUE()
+})
 input.onButtonPressed(Button.A, function () {
     value1 += 1
     DISPLAY_SUM_VALUE()
@@ -16,11 +27,23 @@ function DISPLAY_SUM_VALUE () {
     빼기 = value1 - value2
     곱하기 = value1 * value2
     나누기 = value1 / value2
-    display_text = "" + value1 + "+" + value2 + "=" + sum_value + "      " + value1 + "-" + value2 + "=" + 빼기 + "     " + value1 + "*" + value2 + "=" + 곱하기 + "      " + value1 + "/" + value2 + "=" + 나누기
+    display_text1 = "" + value1 + "+" + value2 + "=" + sum_value
+    display_text2 = "" + value1 + "-" + value2 + "=" + 빼기
+    display_text3 = "" + value1 + "*" + value2 + "=" + 곱하기
+    display_text4 = "" + value1 + "/" + value2 + "=" + 나누기
     OLED.clear()
-    OLED.writeStringNewLine(display_text)
+    OLED.writeStringNewLine(display_text1)
+    OLED.writeStringNewLine("")
+    OLED.writeStringNewLine(display_text2)
+    OLED.writeStringNewLine("")
+    OLED.writeStringNewLine(display_text3)
+    OLED.writeStringNewLine("")
+    OLED.writeStringNewLine(display_text4)
 }
-let display_text = ""
+let display_text4 = ""
+let display_text3 = ""
+let display_text2 = ""
+let display_text1 = ""
 let 나누기 = 0
 let 곱하기 = 0
 let 빼기 = 0
@@ -32,5 +55,25 @@ value1 = 0
 value2 = 0
 DISPLAY_SUM_VALUE()
 basic.forever(function () {
-	
+    if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P0)) {
+        value1 += 2
+        DISPLAY_SUM_VALUE()
+    }
+    if (tinkercademy.ADKeyboard(ADKeys.B, AnalogPin.P0)) {
+        value2 += 2
+        DISPLAY_SUM_VALUE()
+    }
+    if (tinkercademy.ADKeyboard(ADKeys.C, AnalogPin.P0)) {
+        value1 += -2
+        DISPLAY_SUM_VALUE()
+    }
+    if (tinkercademy.ADKeyboard(ADKeys.D, AnalogPin.P0)) {
+        value1 = 0
+        value2 = 0
+        DISPLAY_SUM_VALUE()
+    }
+    if (tinkercademy.ADKeyboard(ADKeys.E, AnalogPin.P0)) {
+        value2 += -2
+        DISPLAY_SUM_VALUE()
+    }
 })
